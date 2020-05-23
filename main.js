5c445d7ae365ea2f3f6f79d18b995f9c04eb997a
Sergio <sergiodelpuertomartin@gmail.com>
2020-05-22 21:05:31 -0700

    Code cleanup

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-function newItem() {
-  var item = document.getElementById("input").value;
-  var ul = document.getElementById("list");
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode("- " + item));
-  ul.appendChild(li);
-  document.getElementById("input").value = "";
-  li.onclick = removeItem;
+const input = document.getElementById("input");
+const list = document.getElementById("list");
+
+input.addEventListener("keyup", function (e) {
+  if (e.keyCode === 13) {
+    addNewItem();
+  }
+});
+
+function addNewItem () {
+  const li = document.createElement("li");
+  li.textContent = input.value;
+  li.addEventListener("click", removeItem);
+  list.appendChild(li);
+  input.value = "";
 }
 
-document.body.onkeyup = function(e) {
-  if (e.keyCode == 13) {
-    newItem();
-  }
-};
-
-function removeItem(e) {
-  e.target.parentElement.removeChild(e.target);
+function removeItem (e) {
+  list.removeChild(e.target);
 }
